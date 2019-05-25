bb00e5584715d97609623eacb2f31d513f49d66e
Tyler Moore <TylerRMoore@gmail.com>
2019-05-25 08:31:53 -0700

    Testing Chart.js

---
+++ Center/uDAQC_Center/uDAQC_WebInterface/uDAQC_charts.js
@@ -0,0 +1,42 @@
+var ctx = document.getElementById('test_chart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: [
+          moment().add(0, 'd').toDate(),
+          moment().add(1, 'd').toDate()
+        ],
+        datasets: [{
+            label: 'My First dataset',
+            fill: false, //no filling under the curve
+            //backgroundColor: 'rgb(0,0,0,0)', //transparent (this fills under the curve)
+            borderColor: 'rgb(255, 0, 0, 255)',
+            data: [
+              1,
+              10
+            ],
+            //pointRadius: 0 //don't render points, but if this is don't you can't hover to get value
+            //pointBackgroundColor: 'rgb(0,0,0,0)',
+            pointBorderColor: 'rgb(0,0,0,0)' //transparent
+        }]
+    },
+
+    // Configuration options go here
+    options:
+    {
+      legend:
+      {
+        display: true,
+        position: 'right'
+      },
+      scales:
+      {
+        xAxes: [{
+          type: 'time'
+        }]
+      }
+    }
+});
