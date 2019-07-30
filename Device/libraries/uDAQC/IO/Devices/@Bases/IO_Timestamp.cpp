ee87d6db6e700258139377d32796ea6ae8e268d2
Tyler Moore <TylerRMoore@gmail.com>
2019-07-30 16:34:34 -0700

    Formatting.

--- Device/libraries/uDAQC/IO/Devices/@Bases/IO_Timestamp.cpp
+++ Device/libraries/uDAQC/IO/Devices/@Bases/IO_Timestamp.cpp
@@ -1,30 +1,32 @@
 #include "IO_Timestamp.h"
 
-namespace ESP_Managers{ namespace IO
+namespace ESP_Managers
 {
-  IO_Timestamp::IO_Timestamp(String name, IO_System* collection):
-  IO_Value<int64_t>(name, (IO_Group*)collection)
+  namespace IO
   {
-    Set(-1); //Initialize the timestamp to -1
-  }
-  void IO_Timestamp::SetTime(timeval tv)
-  {
-    int64_t new_val = (int64_t)(tv.tv_sec)*1000000+(int64_t)(tv.tv_usec);
-    Set(new_val);
-  }
-  void IO_Timestamp::SetTime(int64_t micros)
-  {
-    Set(micros);
-  }
-  void IO_Timestamp::SetTimeToNow()
-  {
-    //The previous method used time of day, but now timestamps will just report micros and the server will calculate the timestamp
+    IO_Timestamp::IO_Timestamp(String name, IO_System* collection):
+    IO_Value<int64_t>(name, (IO_Group*)collection)
+    {
+      Set(-1); //Initialize the timestamp to -1
+    }
+    void IO_Timestamp::SetTime(timeval tv)
+    {
+      int64_t new_val = (int64_t)(tv.tv_sec)*1000000+(int64_t)(tv.tv_usec);
+      Set(new_val);
+    }
+    void IO_Timestamp::SetTime(int64_t micros)
+    {
+      Set(micros);
+    }
+    void IO_Timestamp::SetTimeToNow()
+    {
+      //The previous method used time of day, but now timestamps will just report micros and the server will calculate the timestamp
 
-    //timeval tv;
-    //gettimeofday(&tv,nullptr);
-    //SetTime(tv);
+      //timeval tv;
+      //gettimeofday(&tv,nullptr);
+      //SetTime(tv);
 
-    SetTime((int64_t)micros64());
+      SetTime((int64_t)micros64());
+    }
   }
-}
 };
