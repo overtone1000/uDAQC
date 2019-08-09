63340df9fe1c59ffb5e064e89214444c074fe945
Tyler Moore <TylerRMoore@gmail.com>
2019-08-08 22:39:44 -0700

    Working on deployment.

--- Center/uDAQC_Center/src/main/Console_Center.java
+++ Center/uDAQC_Center/src/main/Console_Center.java
@@ -1,8 +1,14 @@
 package main;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 import udaqc.network.center.Center;
 import udaqc.network.interfaces.CenterHandler;
@@ -31,9 +37,63 @@ public class Console_Center
 			
 		}
 	}
+	
+	private static final String props_file = "properties";
+	private static final String history_key = "history";
 	public static void main(String[] args) {
-		Path path = Paths.get(args[0]);
+		Properties props = new Properties();
+		FileInputStream is=null;
+		try
+		{
+			is = new FileInputStream(props_file);
+			props.load(is);
+		} catch (FileNotFoundException e1)
+		{
+			System.out.println("No property file exists.");
+		} catch (IOException e)
+		{
+			System.out.println("Couldn't read property file.");	
+		}
+		
+		if(is!=null)
+		{
+			try
+			{
+				is.close();
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		FileOutputStream os=null; 
+		if(!props.containsKey(history_key))
+		{
+			props.put(history_key, "history");
+			try
+			{
+				os = new FileOutputStream(props_file);
+				props.store(os, null);
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		if(os!=null)
+		{
+			try
+			{
+				os.close();
+			} catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 				
+		String path_str = props.getProperty(history_key);
+		Path path = Paths.get(path_str);
+		
 		DummyHandler h = new DummyHandler();
 		
 		@SuppressWarnings("unused")
